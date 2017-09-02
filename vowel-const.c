f69459c24c11904196a41309ba890b1ab362dc5f
monica9797 <31558994+monica9797@users.noreply.github.com>
2017-09-02 11:48:16 +0530

    Create vowel-const.c

---
+++ vowel-const.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ char c;
+ if(c=='a' || c=='e'|| c=='i'||c=='o'||c=='u')
+ {
+  printf("vowel");
+  }
+  else
+  {
+  printf("consonant");
+  }
+ }
