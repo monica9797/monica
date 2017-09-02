7a4dd6846ee960bd30be354848fb09495c3373ce
monica9797 <31558994+monica9797@users.noreply.github.com>
2017-09-02 12:18:48 +0530

    Update and rename alpha-not.c to alph or not.c

---
+++ alph or not.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ char c;
+ if((c>='a' && 'c<=z') ||  (c>='A'  &&  c<='Z'))
+ {
+   printf("Alphabet");
+  }
+ else
+ {
+  printf("not a alphabet");
+  }
+}
+  
