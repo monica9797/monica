c515918c7956c48a8422382b6e9563725c54769b
monica9797 <31558994+monica9797@users.noreply.github.com>
2017-09-02 11:07:54 +0530

    Create check.c

---
+++ check.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n;
+if(n>0)
+{
+  printf("positive");
+}
+ else if(n<0)
+{ 
+  printf("negative");
+}
+else
+{
+   printf("neutral");
+}
+}
