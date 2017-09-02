dfb66ada8f434947934207fb3f0621c74a4fdbd1
monica9797 <31558994+monica9797@users.noreply.github.com>
2017-09-02 12:25:01 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+if(a>b)
+{
+ printf("%d",a);
+ }
+ else if(b>c)
+ {
+  printf("%d",b);
+  }
+ else
+ {
+   printf("%d",C);
+   }
+ }
